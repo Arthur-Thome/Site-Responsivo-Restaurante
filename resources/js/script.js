c87c434d73d4c399f27057d6abe08d4748819de1
ARTHUR DE AZEVEDO THOME <771600337@fspoa.br>
2024-10-22 21:53:14 -0300

    Menu Responsivo

---
+++ resources/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(
+    function(){
+        $('.js--mobile-nav').click(
+            function(){
+                var nav = $('.js--main-nav')
+                nav.slideToggler(300);
+            }
+        );
+    }
+);
